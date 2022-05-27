169046c2dc35ccc16b60ddf39923162bbbe49926
Karpov Yuri <yurick@bk.ru>
2022-05-27 15:21:35 +0300

    fix: Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
-function Header() {
+import { Link, Route } from 'react-router-dom';
+
+function Header({ loggedIn }) {
   return (
     <header className="header">
       <a href="#" aria-label="Логотип Mesto Russia" className="header__logo fade-opacity"></a>
+      <Route path="/sign-in">
+        <Link to='sign-up'
+          className={`${loggedIn ? 'header__link' : 'header__link header__link_login'}`}>
+          Регистрация
+        </Link>
+      </Route>
+      <Route path="/sign-up">
+        <Link to='sign-in'
+          className={`${loggedIn ? 'header__link' : 'header__link header__link_login'}`}>
+          Войти
+        </Link>
+      </Route>
     </header>
   );
 }
